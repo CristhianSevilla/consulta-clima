65584c50bba0c29dbe72bba2562273cac80d8e03
Cristhian Sevilla <crissev17@gmail.com>
2023-06-01 17:41:56 -0600

    Creando el provider

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -5,10 +5,27 @@ const ClimaContext = createContext()
 
 const ClimaProvider = ({ children }) => {
 
+    const [busqueda, setBusqueda] = useState(
+        {
+            ciudad: '',
+            pais: '',
+        }
+    )
+
+    const datosBusqueda = e => {
+        setBusqueda(
+            {
+                ...busqueda,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
     return (
         <ClimaContext.Provider
             value={{
-
+                busqueda,
+                datosBusqueda
             }}
         >
             {children}
